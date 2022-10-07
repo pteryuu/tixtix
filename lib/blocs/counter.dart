93e9b45ac787f843bb0b2887b8c50c994b4bfa77
Pieter Yu <yiupieter@gmail.com>
2022-10-08 00:23:31 +0700

    Penambahan stream builder dan diperlukan revisi terhadap perpindahan halaman

--- lib/blocs/counter.dart
+++ lib/blocs/counter.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class CounterBloc {
   int _counter = 0;
 
@@ -35,5 +37,26 @@ class CounterBloc {
   void dispose() {
     _inputController.close();
     _outputController.close();
+  }
+}
+
+class CubitCounter extends Cubit<int> {
+  CubitCounter({this.init = 0}) : super(init);
+
+  int init;
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print(error);
   }
 }
